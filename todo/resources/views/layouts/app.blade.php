5f5c2605072340189c55e61ad8bb6e8a35f43b84
Raihan <raihanromi98@gmail.com>
2024-09-15 23:38:07 +0600

    header link added

--- todo/resources/views/layouts/app.blade.php
+++ todo/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 <body class="bg-gray-100 min-h-screen">
     <div class="container mx-auto px-4 py-8">
         <header class="mb-8">
-            <h1 class="text-3xl font-bold text-gray-800">Todo App</h1>
+            <a href="/todos" class="text-3xl font-bold text-gray-800">Todo App</a>
         </header>
         <main>
             @yield('content')
